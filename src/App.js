35e81feedf95da0a01b339f183659a876881fb37
casey harding <casey.harding@generalassemb.ly>
2021-03-13 15:05:22 -0500

    Updated login flow, added Profile route.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, withRouter} from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
-import Login from './Login'
-import Signin from './Signin'
+import Login from './Login';
+import Signin from './Signin';
+import Profile from './Profile';
 
 class App extends Component {
   constructor(props) {
@@ -19,21 +20,35 @@ class App extends Component {
     }
   }
 
+  login = async (e, user) => {
+    e.preventDefault();
+    const data = {
+      username: user.username,
+      password: user.password,
+    };
+    console.log(data)
+    const response = await axios.post('http://localhost:3001/users/login', data);
+    this.props.history.push('/Profile');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to Cheap Travel (non-tourist)</h1>
         <Link to="/login">Login</Link>
         <Route path="/login" render={() => (
-            <Login />
+            <Login login={this.login} />
           )} />
         <Link to="/signin">Signin</Link>
         <Route path="/signin" render={() => (
             <Signin />
+        )} />
+        <Route path="/Profile" render={() => (
+          <Profile />
         )} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
