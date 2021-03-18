735c6a6c1d545d0d2f4d0cfa797ba8172e0aced5
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-03-18 08:44:47 -0700

    home

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,6 @@ const Home = () => {
           <h1>Welcome to Cheap Travel (non-tourist)</h1>
         </div>  
     )     
-
-
-// render (
-//     https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_82631380_travel-budget-concept-money-saved-for-vacation-in-glass-jar-on-world-map-background-copy-space-bankn.html&psig=AOvVaw1jeEGOQbC13loeIpwHqVkU&ust=1616165568537000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCkkZmMuu8CFQAAAAAdAAAAABAD
-// )
     }
 
 export default (Home);
